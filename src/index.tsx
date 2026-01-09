202d375db21dd51fed1422f7be3a45348b2398c4
Jori Lindell <jori.lindell@gmail.com>
2026-01-09 14:04:49 +0200

    feat: implement routing with AppRoutes component and update index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import 'core-js/internals/object-assign.js';
 import React from 'react';
 import { createRoot, Root } from 'react-dom/client';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { compose, applyMiddleware, legacy_createStore as createStore, Store, StoreEnhancer } from 'redux';
 import { Provider } from 'react-redux';
 import promiseMiddleware from 'redux-promise';
@@ -14,20 +14,7 @@ import queueHandler from './actions/queueHandler';
 
 import rootReducer from './reducers/index';
 
-import App from './components/Main';
-
-import DashBoard from './components/DashBoard';
-import GroupList from './components/GroupList';
-import UnitList from './components/UnitList';
-import UnitDetails from './components/UnitDetails';
-import UnitHistory from './components/UnitHistory';
-import UpdateConfirmation from './components/UpdateConfirmation';
-import DeleteConfirmation from './components/DeleteConfirmation';
-import UpdateQueue from './components/UpdateQueue';
-import LoginScreen from './components/LoginScreen';
-import NotFound from './components/NotFound';
-import UnitMassEdit from './components/UnitMassEdit';
-import UnitMassEditPropertySelect from './components/UnitMassEditPropertySelect';
+import AppRoutes from './components/AppRoutes';
 
 import moment from 'moment';
 
@@ -53,13 +40,13 @@ const persistConfig: PersistConfig<RootState> = {
     {
       in: (state: any, key: string) => {
         if (key === 'data' && state) {
-          // eslint-disable-next-line no-unused-vars
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
           const { loading, ...stateWithoutLoading } = state;
           return stateWithoutLoading;
         }
         return state;
       },
-      // eslint-disable-next-line no-unused-vars
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       out: (state: any, key: string) => state
     }
   ]
@@ -68,7 +55,7 @@ const persistConfig: PersistConfig<RootState> = {
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // Fix for minification issues: When Redux DevTools Extension is not available,
-// the && operator returns undefined which gets passed to compose(). 
+// the && operator returns undefined which gets passed to compose().
 // When minified, this becomes 'b(...args)' where 'b' is undefined, causing "b is not a function" error.
 // This approach conditionally adds enhancers to avoid passing undefined to compose().
 const enhancers: StoreEnhancer[] = [applyMiddleware(promiseMiddleware)];
@@ -97,22 +84,7 @@ root.render(
   <Provider store={store}>
     <PersistGate loading={<div>Ladataan...</div>} persistor={persistor}>
       <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<LoginScreen />} />
-          <Route path="/" element={<App />}>
-            <Route path="/" element={<DashBoard />} />
-            <Route path="/group" element={<GroupList />} />
-            <Route path="/group/:groupId" element={<UnitList />} />
-            <Route path="/group/:groupId/mass-edit" element={<UnitMassEditPropertySelect />} />
-            <Route path="/group/:groupId/mass-edit/:propertyId" element={<UnitMassEdit />} />
-            <Route path="/unit/:unitId" element={<UnitDetails />} />
-            <Route path="/unit/:unitId/history" element={<UnitHistory />} />
-            <Route path="/unit/:unitId/update/:propertyId/:valueId" element={<UpdateConfirmation />} />
-            <Route path="/unit/:unitId/delete/:propertyId" element={<DeleteConfirmation />} />
-            <Route path="/queue" element={<UpdateQueue />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
+        <AppRoutes />
       </BrowserRouter>
     </PersistGate>
   </Provider>
